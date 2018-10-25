56c347cc17ae96ec08cbcf7c38f45b0b5bedce8a
Akil Howard <akil.howard1@gmail.com>
2018-10-25 13:38:17 -0400

    newtest

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.getElementById("app").innerHTML = `
 <h1>Hello Parcel!</h1>
 <h1>Test</h1>
+<h1>Test</h1>
 <div>
   Look
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
